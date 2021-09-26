2d267d502301d37753e22356bf006737342cd36c
Jay Anton Roblico <jayantonroblico23@gmail.com>
2021-09-27 06:25:04 +0800

    click event

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,28 @@
 <template>
     <header>
         <h1>{{title}}</h1>
+        <Button text="Add task" color="green" />
     </header>
 </template>
 
 <script>
+import Button from './Button.vue'
+
 export default {
     name: 'Header',
     props:{
         title: String
-    }
+    },
+    components:{
+        Button
+    },
 }
 </script>
 
 <style scoped>
 header{
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     margin-bottom: 20px;
 }
